ea176d02015f6890439140820c49982595a94587
LeightonSkervin <lskervin23@gmail.com>
2024-03-28 13:15:14 -0400

    lab complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as log_file:
+        log_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode='a', encoding='utf-8') as log_file:
+        log_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_content = ""
+    with open(f'{file_name}.txt', encoding='utf-8') as log_file:
+        for line in log_file:
+            # Concatenate each line to the file_content string
+            file_content += line
+    return file_content
